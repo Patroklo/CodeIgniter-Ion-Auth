d3f79956820b6a923b4d46cb6df214774cab1e43
root <root@patroklo.cu.cc>
2014-08-21 12:16:05 -0400

    changed controller methods

--- controllers/Auth_controller.php
+++ controllers/Auth_controller.php
@@ -737,38 +737,6 @@ class Auth_controller extends MY_Controller {
 		}
 	}
 
-
-
-
-/*	function _get_csrf_nonce()
-	{
-		$this->load->helper('string');
-		$key   = random_string('alnum', 8);
-		$value = random_string('alnum', 20);
-		$this->session->set_flashdata('csrfkey', $key);
-		$this->session->set_flashdata('csrfvalue', $value);
-
-		return array($key => $value);
-	}
-
-	function _valid_csrf_nonce()
-	{
-		
-			echo '<pre>';
-			  echo var_dump($_POST);
-			echo '</pre>';
-			die();
-		if ($this->input->post($this->session->flashdata('csrfkey')) !== FALSE &&
-			$this->input->post($this->session->flashdata('csrfkey')) == $this->session->flashdata('csrfvalue'))
-		{
-			return TRUE;
-		}
-		else
-		{
-			return FALSE;
-		}
-	}
-*/
 	function _render_page($view, $data=null, $render=false)
 	{
 
